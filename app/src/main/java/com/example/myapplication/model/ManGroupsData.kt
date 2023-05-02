0b0936dfb4c9d5155c388ad749218819a5256651
Plovotok <z-sozinov@mail.ru>
2023-05-02 18:17:40 +0300

    updating data

--- app/src/main/java/com/example/myapplication/model/ManGroupsData.kt
+++ app/src/main/java/com/example/myapplication/model/ManGroupsData.kt
@@ -5,11 +5,34 @@ import com.example.myapplication.R
 class ManGroupsData() {
     companion object{
         val dataMap = mapOf(
-            "Hyungwon" to R.drawable.mx_heinvon_ready,
-            "I.M." to R.drawable.mx_im_ready,
-            "Joohoney" to R.drawable.mx_juhoni_ready,
-            "Minhyuk" to R.drawable.mx_minheik_ready,
-            "Kihyun" to R.drawable.mx_kihein_ready)
+
+            "Jey Houp" to R.drawable.bts_jey_houp,
+            "Jimin" to R.drawable.bts_jimin,
+            "Jin" to R.drawable.bts_jin,
+            "Jonguk" to R.drawable.bts_jongik,
+            "RM" to R.drawable.bts_rm,
+            "Shuga" to R.drawable.bts_shuga,
+            "V" to R.drawable.bts_v,
+
+            "Huyngwon" to R.drawable.mx_hyungwon,
+            "I.M" to R.drawable.mx_im,
+            "Joohoney" to R.drawable.mx_joohoney,
+            "Kihyun" to R.drawable.mx_kihyun,
+            "Minhyuk" to R.drawable.mx_minhyuk,
+            "Shownu" to R.drawable.mx_shownu,
+            "Wonho" to R.drawable.mx_wonho,
+
+            "Bang Chan" to R.drawable.sk_bang_chan,
+            "Changbin" to R.drawable.sk_changbin,
+            "Felix" to R.drawable.sk_felix,
+            "Han" to R.drawable.sk_han,
+            "Huynjin" to R.drawable.sk_hyunjin,
+            "I.N" to R.drawable.sk_in,
+            "Lee Know" to R.drawable.sk_lee_know,
+            "Seungmin" to R.drawable.sk_seungmin,
+            "Woojin" to R.drawable.sk_woojin
+
+            )
     }
 
 }
